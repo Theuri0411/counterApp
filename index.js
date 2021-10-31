aab35df9efe389edaed47d42d7a08b237624065c
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-10-31 23:28:00 +0300

    UPDATE

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-document.getElementById("count-el").innerText = 5;
+// document.getElementById("count-el").innerText = 5;
+// let firstBatch = 5
+// let secondBatch = 7
+
+
+// let count = firstBatch + secondBatch
+
+// console.log(count)
+
+let count = 5
+
+count = count + 1
+
+console.log(count)
