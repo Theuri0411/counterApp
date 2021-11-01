577187896f225b447a7a99a1131a35324c7c08e6
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-11-02 00:13:20 +0300

    final update

--- index.js
+++ index.js
@@ -43,16 +43,14 @@ let count = 0
 
 function increment (){
     count += 1
-    countEl.innerText= count
+    countEl.textContent= count
 
     console.log (count)
 }
 
 function save() {
     let countStr = count + " - "
-    saveEl.innerText += countStr
-
-
-
-    console.log("count")
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count=0
 } 
